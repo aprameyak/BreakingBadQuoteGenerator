4ce170ccb08237fb526252a204dc2018b8768593
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-01-25 22:17:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+'use client'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  async function getQuote() {
+    try {
+      const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      setQuote(json[0].quote);
+      setAuthor(json[0].author);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1 className={styles.h}>
+        Breaking Bad Quote Generator
+      </h1>
+      <button className={styles.but} onClick={getQuote}>
+        Generate
+      </button>
+      <h1 className={styles.h}>
+        {quote}
+      </h1>
+      <h1 className={styles.h}>
+        {author}
+      </h1>
     </div>
   );
 }
-
-export default App;
