aef2bffce3c2a2fa9815b90d77658c6fbb9f11bb
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-01-25 22:18:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./App.css";
 import { useState } from "react";
 
 export default function App() {
